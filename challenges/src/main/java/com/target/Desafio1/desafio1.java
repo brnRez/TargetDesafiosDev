8089fd9a8afe3439bc116d0d4ac41d20cfe2c418
Bruno Maciel <maciel2bruno@gmail.com>
2025-05-10 03:09:26 -0300

    feat: solved challenge1

---
+++ challenges/src/main/java/com/target/Desafio1/desafio1.java
@@ -0,0 +1,22 @@
+package com.target.Desafio1;
+
+public class desafio1 {
+    
+    
+    public static void main(String[] args) {
+    
+    int indice = 13;
+    int soma = 0;
+    int k = 0;
+
+    while (k < indice) {
+        k+=1;
+        soma = soma + k;
+    }
+
+    System.out.println("Valor Final da Soma: " + soma);
+
+    
+    }
+    
+}
